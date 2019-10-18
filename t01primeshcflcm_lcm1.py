75899fada6dd462365875745d1b28cd9262be9df
dhswuzijun <37992214+dhswuzijun@users.noreply.github.com>
2019-10-18 14:32:12 +0800

    Update t01primeshcflcm_lcm1.py

--- t01primeshcflcm_lcm1.py
+++ t01primeshcflcm_lcm1.py
@@ -1,2 +1,22 @@
 # Find the Lowest Common Multiple (LCM) of 18 and 30.
 
+# Get the list of factors of b
+for i in range(2,b+1):
+  if b % == 0:
+    factors_b + [i]
+    
+print(factors_a)
+print(fators_b)
+
+common_factors = []
+for i in factors_a:
+  if i in factors_b:
+    common_factors = common_factors + [i]
+    
+print(common_factors)
+
+hcf = max(common_factors)
+print(hcf)
+
+lcm = a * b / hcf
+print('lowest common multiple of",a,"and",b."is:". int(lcm))
